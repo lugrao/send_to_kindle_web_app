5a2c590329a74285cd61944004e034040bde061c
e <elugrao@gmail.com>
2023-03-06 17:39:10 -0300

    add html to kindle feature

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from html2epub2kindle import html2epub2kindle
 
 app = Flask(__name__)
 
@@ -6,3 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/<path:url>")
+def html2kindle(url):
+    html2epub2kindle(url)
+    return "The article was sent to your kindle. Apparently."
