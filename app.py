6b4d0fae1b8228b17c0ec311aa1501290d674bac
e <elugrao@gmail.com>
2023-03-06 18:42:57 -0300

    add form submission handling

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_login import LoginManager
 from html2epub2kindle import html2epub2kindle
+import os
 
+login_manager = LoginManager()
 app = Flask(__name__)
 
 
@@ -9,7 +12,16 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/<path:url>")
-def html2kindle(url):
+@app.route("/url", methods=['POST'])
+def html2kindle():
+    url = request.form.get("url")
+    password = request.form.get("password")
+
+    if not url:
+        return "You must provide a URL."
+    
+    if password != os.environ.get("APP_PASSWORD"):
+        return "Incorrect password."
+
     html2epub2kindle(url)
     return "The article was sent to your kindle. Apparently."
