b5b7176ed8d44cc4edaacd4cd2c003f069605ca9
lugrao <elugrao@gmail.com>
2023-05-15 13:19:16 -0300

    set text encoding

--- html2epub2kindle.py
+++ html2epub2kindle.py
@@ -29,6 +29,8 @@ def html2epub2kindle(url):
                   " Firefox/89.0")
     res = requests.get(url, headers={"User-Agent": user_agent})
 
+    res.encoding = res.apparent_encoding
+
     html_article = Document(res.text)
 
     # Get clean filename
